8f7056e98f3257aeeb84ae340be9c4553c4fcf12
Alex Ruger <alexrugermusic@gmail.com>
2024-09-19 14:19:34 -0700

    Remade go.mod

--- co.go
+++ co.go
@@ -1,10 +1,10 @@
 package cogo
 
 import (
-    // "github.com/rewgs/cogo/internal/base"
+    "github.com/rewgs/cogo/internal/base"
 )
 
-// type currentMachine struct {
-//     Name            string
-//     SyncMethods     []base.SyncMethod
-// }
+type currentMachine struct {
+    Name            string
+    SyncMethods     []base.SyncMethod
+}
